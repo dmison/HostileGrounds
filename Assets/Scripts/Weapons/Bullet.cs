8a46487786e6f48d2075b7f79279fd7c279003fd
Dana Mison <dana.jadzia.mison@gmail.com>
2025-05-31 19:40:15 +1000

    Bullets were not removing themselves

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -18,7 +18,7 @@ namespace Weapons
         private IEnumerator DestroyBullet (float delay)
         {
             yield return new WaitForSeconds(delay);
-            Destroy(this);
+            Destroy(gameObject);
         }
 
         private void OnCollisionEnter(Collision collision)
